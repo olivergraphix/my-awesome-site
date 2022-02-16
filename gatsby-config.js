a1ca676091733443818b39d663b986b65e1b8e66
olivergraphix <webmaster@graphixvip.com>
2022-02-15 21:03:23 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-      title: `My Awesome Site`,
+      title: `This is cool!!!`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
@@ -8,12 +8,21 @@ module.exports = {
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp",
+    // {
+    // resolve: 'gatsby-source-filesystem',
+    // options: {
+    //   "name": "images",
+    //   "path": "./src/images/"
+    // },
+    //     __key: "images"
+    // },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "blog",
+        "path": `${__dirname}/blog`,
+      },
     },
-    __key: "images"
-  }]
+  ]
 };
