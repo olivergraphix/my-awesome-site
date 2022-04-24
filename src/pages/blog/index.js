6cc8775d2bce80369c7a68f9c97e5935a0392828
olivergraphix <webmaster@graphixvip.com>
2022-04-23 21:04:43 -0400

    Again 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -4,7 +4,7 @@ import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
     return (
-        <Layout pageTitle="My Blog Posts are Here">
+        <Layout pageTitle="My Blog Posts are Here!!">
             {
                 data.allMdx.nodes.map(node => (
                     <article key={node.id}>
